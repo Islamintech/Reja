d4f413a90bb33fd2348da02012649884bfe13aaa
David <ergashevisloombek@gmail.com>
2025-12-25 01:42:02 +0900

    BRR: building express web server

---
+++ server.js
@@ -0,0 +1,35 @@
+console.log("Web server boshlash");
+const express = require('express');
+const app = express();
+const http = require('http');
+
+//1 Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//2 Session code
+
+//3 Views code
+app.set("views", "views");
+app.set("view engine", "ejs"); 
+
+//4 Routing code
+app.get("/", function(req, res){
+    res.send(`<h1 style="background: red">Hello World!!!</h1>`);
+});
+
+app.get("/hello", function(req, res){
+    res.send(`<h1 style="background: red">Salom user</h1>`);
+});
+
+app.get("/gift", function(req, res){
+    res.send(`<h1 style="background: red">You are on gifts page</h1>`);
+});
+
+
+const server = http.createServer(app);
+const PORT = 3000;
+server.listen(PORT,function(){
+    console.log(`The server is running successfully on port: ${PORT}`);
+});
