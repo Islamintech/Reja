3d104b0b7bff13add6135e9acaf29cc0a7b8ec85
David <ergashevisloombek@gmail.com>
2025-12-27 19:25:58 +0900

    Feat: Fixed the bug

--- server.js
+++ server.js
@@ -1,42 +1,44 @@
-console.log("Web server boshlash");
-const express = require('express');
+console.log("Web serverni boshlash");
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const fs = require("fs");
 
 let user;
-fs.readFile("database/user.json", "utf8", (err, data)=>{
-    if(err){
-        console.log("ERROR:", err);
-    }else{
-        user=JSON.parse(data);
-    }
-})
-
-//1 Kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-//2 Session code
-
-//3 Views code
-app.set("views", "views");
-app.set("view engine", "ejs"); 
-
-//4 Routing code
-
-app.get("/author", function(req, res){
-    res.render("author", {user: user});
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
 });
 
-app.get("/", function(req, res){
-    res.render("harid")
-})
+//1 KIRISH CODES. kirish malumotlarga bogliq bolgan kodlar yoziladi
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+//2 session CODES
+
+//3 VIEWS CODES
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+//4 ROUTING CODES
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  res.json({ test: "success" });
+  // Ma'lumotlarni qabul qilish
+});
+app.get("/auther", (req, res) => {
+  res.render("auther", { user: user });
+});
+app.get("/", function (req, res) {
+  res.render("harid");
+});
 
 const server = http.createServer(app);
-const PORT = 3000;
-server.listen(PORT,function(){
-    console.log(`The server is running successfully on port: ${PORT}`);
+let PORT = 3000;
+server.listen(PORT, function () {
+  console.log(`Server is running successfully on port: ${PORT}`);
 });
