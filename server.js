dd76971c1d441f2b3b06f90d2bb8074933e76bcd
David <ergashevisloombek@gmail.com>
2025-12-27 18:01:50 +0900

    feat: I have created author.ejs

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ console.log("Web server boshlash");
 const express = require('express');
 const app = express();
 const http = require('http');
+const { userInfo } = require('os');
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data)=>{
+    if(err){
+        console.log("ERROR:", err);
+    }else{
+        user=JSON.parse(data);
+    }
+})
 
 //1 Kirish code
 app.use(express.static("public"));
@@ -17,6 +28,10 @@ app.set("view engine", "ejs");
 //4 Routing code
 app.get("/", function(req, res){
     res.render("harid")
+})
+
+app.get("/author", function(req, res){
+    res.render("author", {user: user});
 })
 
 app.post("/create", (req, res) => {
