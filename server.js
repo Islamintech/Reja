12550365103fa887bd93a8f9cf5311534f36b826
David <ergashevisloombek@gmail.com>
2025-12-27 19:47:28 +0900

    feat: Fixed the bug

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ fs.readFile("database/user.json", "utf-8", (err, data) => {
   }
 });
 
-//1 KIRISH CODES. kirish malumotlarga bogliq bolgan kodlar yoziladi
+//1 KIRISH CODE
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -25,13 +25,8 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 //4 ROUTING CODES
-app.post("/create-item", (req, res) => {
-  console.log(req.body);
-  res.json({ test: "success" });
-  // Ma'lumotlarni qabul qilish
-});
-app.get("/auther", (req, res) => {
-  res.render("auther", { user: user });
+app.get("/author", (req, res) => {
+  res.render("author.ejs", { user: user });
 });
 app.get("/", function (req, res) {
   res.render("harid");
